406911c459b56d619958571d8568288db032c3c4
Ibragimov Khusanboy <mail4khusanboy@gmail.com>
2018-01-24 12:51:19 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -6,10 +6,10 @@ if (index.name === "Joe") {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
-var index = require("./index.js")
+
 
 if (index.height === 74) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 70, Received: ")
 }

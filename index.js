755932fa66b445408bd5c6b1b90cf8de088250af
Ibragimov Khusanboy <mail4khusanboy@gmail.com>
2018-01-24 12:26:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan"){
   consonle.log("The name is correct")
+}else{
+  consonle.log("Expected the name to be Susan")
 }
 
 // Don't worry about this
